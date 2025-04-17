9a14449d41d8f2719f106c039d02a1f67a742294
AymenCity <91978242+AymenCity@users.noreply.github.com>
2025-04-17 22:43:51 +0100

    Sidepanel api

--- background.js
+++ background.js
@@ -1,7 +1,29 @@
 // https://www.youtube.com/watch?v=Jxj_jfh4IDk
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({
-      url: chrome.extension.getURL('popup.html'),
-      active: true
-    });
+chrome.action.onClicked.addListener(function(tab) {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL()('popup.html'),
+    active: true
   });
+});
+
+const GOOGLE_ORIGIN = 'https://mail.google.com';
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on google.com
+  if (url.origin === GOOGLE_ORIGIN) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: 'popup.html',
+      enabled: true
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false
+    });
+  }
+});
+
